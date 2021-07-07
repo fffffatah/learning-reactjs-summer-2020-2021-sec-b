4aad84cbffe6de4ff4dd36783e57041bf8f1f8a1
A.F.M. Noorullah <ab.fatahmn@gmail.com>
2021-07-07 23:59:29 +0600

    ES6 syntax and classes

---
+++ Lab 1/Student.js
@@ -0,0 +1,25 @@
+exports class Student{
+	
+	constructor(id, name, email){
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	getName(){
+		return this.name;
+	}
+}
+
+exports class User{
+	
+	constructor(id, name, email){
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	getName(){
+		return this.name;
+	}
+}
